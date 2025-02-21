109517a011258a24b4dc81f36573f9487ea57b5c
Eslam358 <you@example.com>
2025-02-21 09:28:01 +0200

    add

--- src/utils/axiosCreat.ts
+++ src/utils/axiosCreat.ts
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 // export const BASE_URL = "http://localhost:3000" locale
-export const BASE_URL = "https://app-articles-jbhb2r6i2-eslams-projects-a656fcfb.vercel.app"
+export const BASE_URL = "https://app-articles.vercel.app"
+
 
 export default axios.create({
   baseURL: BASE_URL,
